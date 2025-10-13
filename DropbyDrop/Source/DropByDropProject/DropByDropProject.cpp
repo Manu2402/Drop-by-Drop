e319f6cc0e2c6960f5839fbcfc14da3f682951ad
Manu2402 <solanomanuel002@gmail.com>
2025-10-13 13:41:25 +0200

    Converted from project folder to plugin folder

--- DropbyDrop/Source/DropByDropProject/DropByDropProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DropByDropProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DropByDropProject, "DropByDropProject" );
